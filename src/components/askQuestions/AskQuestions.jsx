54d26956c647d99ab7cdc54cff64d7dee19bd1ca
mfonmfon <mfonm579@gmail.com>
2024-12-31 09:19:30 +0100

    pushing right now

--- src/components/askQuestions/AskQuestions.jsx
+++ src/components/askQuestions/AskQuestions.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import AskQuestionStyle from '../../styles/AskQuestion.module.css'
+
 const AskQuestions = () => {
   return (
     <div className={AskQuestionStyle.container}>
+      <div className={AskQuestionStyle.splitHeader}>
       <div className={AskQuestionStyle.contentHeader}>
       <h1>Do You Have Question</h1>
       <p>We'll help connect you to very good technician</p>
-      <div className={AskQuestionStyle.contact}>
-        <button className={AskQuestionStyle.question}>Contact</button>
-      </div>
     </div>
+
+    <div className={AskQuestionStyle.contact}>
+        <button className={AskQuestionStyle.question}>Contact</button>
+    </div>
+      </div>
   </div>
   )
 }
